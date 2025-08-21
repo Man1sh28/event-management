3e4d1f3dfc1d1b77760620cafe020131352ab78a
Man1sh28 <man1shbarik.fr@gmail.com>
2025-08-21 08:21:38 +0530

    port errors

--- app.py
+++ app.py
@@ -835,7 +835,7 @@ def delete_all_data():
     
     flash('All data has been deleted successfully!', 'success')
     return redirect(url_for('dashboard'))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_db()
-    app.run(debug=True, port=8001)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
